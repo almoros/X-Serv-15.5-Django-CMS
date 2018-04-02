e1784fd348272b816e7b1584b147c7d2c82b8156
almoros <javieraf88@gmail.com>
2018-04-02 19:26:53 +0200

    Versión 1

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from cms.models import Pages
 # Create your views here.
+
+def pagina_inicial(request):
+    respuesta = "Esto es la página inicial,"
+    respuesta += " y estos son los recursos que hay:"
+    lista = Pages.objects.all()
+    for pagina in lista:
+        respuesta += '<br><a href="/page/' + str(pagina.id) + '">' + pagina.name + '</a>'
+
+    return HttpResponse(respuesta)
+
+def pagina(request,identificador):
+    try:
+        pagina = Pages.objects.get(id = identificador)
+        respuesta = "Has introducido " + str(pagina.id) + " que corresponde a: " + str(pagina.page) + " y su id es: " + str(pagina.id)
+    except Pages.DoesNotExist:
+        respuesta = "No existe la pagina con el identificador " + str(identificador)
+
+    return HttpResponse(respuesta)
